bb9b3d1bb4bf1da2478eeab3f3ff6c14675a5cf3
mmccall0813 <34384161+mmccall0813@users.noreply.github.com>
2018-10-12 09:23:55 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Oct 13, 2018").getTime();
+var countDownDate = new Date("Oct 12, 2018 14:15:00").getTime();
 var x = setInterval(function() {
 var now = new Date().getTime();    
 var distance = countDownDate - now;    
